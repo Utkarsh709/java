fddb2bee401b994a9830f052a8a6747683fce2d5
utkarsh709 <utkarshvinay74@gmail.com>
2024-01-12 19:53:34 +0530

    adding modulus function

--- calci.java
+++ calci.java
@@ -1,5 +1,6 @@
 //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
 //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
+//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
 
 class calci
 {
@@ -13,9 +14,9 @@ int add=addition(int x,int y):   //adding addition function
 
 int sub=subtraction(int x,int y);   //adding subtraction function
 
-int div=divison(int x,int y);    //adding divison function
+int div=divison(int x,int y); //adding divison function
 
-
+int mod=modulus(int x,int y);   //adding modulus function
 }
 
 
@@ -35,6 +36,10 @@ return a-b;
 
 static int divison(int a,int b){
 return a/b;
+}
+
+static int modulus(int a,int b){
+return a%b;
 }
 
 
