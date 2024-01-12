442deeed6e0a17de6ce8a5936da1ea709ca18d8d
utkarsh709 <utkarshvinay74@gmail.com>
2024-01-12 18:50:58 +0530

    adding multiply function

--- calci.java
+++ calci.java
@@ -1,6 +1,21 @@
+//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
+
 class calci
 {
-public:
-int x=10
-int y=20
+public static void main(string args []){
+int x=10;
+int y=20;
+int mult=multiply(int x,int y);  //adding multiply function
 }
+
+static int multiply(int a,int b){
+return a*b;
+}
+
+
+
+
+
+
+
+
