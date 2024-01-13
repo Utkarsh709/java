85e3d83c049184afa0d071ef418d6ecccdb50c65
utkarsh709 <utkarshvinay74@gmail.com>
2024-01-13 07:19:10 +0530

    added square function

--- calci.java
+++ calci.java
@@ -2,21 +2,26 @@
 //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
 //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
 
-class calci
-{
+class calci{
+
 public static void main(string args []){
 int x=10;
 int y=20;
 
 int mult=multiply(int x,int y);  //adding multiply function
 
-int add=addition(int x,int y):   //adding addition function
+int add=addition(int x,int y);   //adding addition function
 
 int sub=subtraction(int x,int y);   //adding subtraction function
 
 int div=divison(int x,int y); //adding divison function
 
 int mod=modulus(int x,int y);   //adding modulus function
+
+int sq=square(int x);    //adding square function
+
+
+
 }
 
 
@@ -42,5 +47,9 @@ static int modulus(int a,int b){
 return a%b;
 }
 
+static int square(int a){
+return a*a;
+}
 
 
+}
