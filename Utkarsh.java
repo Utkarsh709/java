19f668522fadb1ec74877a720b00b1a880e2fb78
Utkarsh709 <155797679+Utkarsh709@users.noreply.github.com>
2024-03-19 22:36:09 +0530

    pattern printing

---
+++ Utkarsh.java
@@ -0,0 +1,149 @@
+class Utkarsh{
+
+static void U(){
+  clrscr();
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("******************");
+  System.out.println("******************");
+
+}
+
+static void T(){
+  clrscr();
+  System.out.println("******************");           
+  System.out.println("******************");            
+  System.out.println("******************");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+  System.out.println("       ****       ");
+ 
+
+}
+
+static void K(){
+  clrscr();
+  System.out.println("***               ");
+  System.out.println("***            ***");
+  System.out.println("***         ***   ");
+  System.out.println("***      ***      ");
+  System.out.println("***    ***        ");
+  System.out.println("*** ***           ");
+  System.out.println("*** ***           ");
+  System.out.println("***    ***        ");
+  System.out.println("***      ***      ");
+  System.out.println("***        ***    ");
+  System.out.println("***          ***  ");
+  System.out.println("***            ***");
+  System.out.println("***               ");
+
+}
+
+
+static void A(){
+  clrscr();
+  System.out.println("   ***********    ");
+  System.out.println(" **************** ");
+  System.out.println("******************");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("******************");
+  System.out.println("******************");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+
+}
+
+
+static void R(){
+  clrscr();
+  System.out.println("******************");
+  System.out.println("******************");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("******************");
+  System.out.println("******************");
+  System.out.println("******            ");
+  System.out.println("*** ***           ");
+  System.out.println("***    ***        ");
+  System.out.println("***       ***     ");
+  System.out.println("***          ***  ");
+  System.out.println("***            ***");
+
+}
+
+static void S(){
+  clrscr();
+  System.out.println("******************");
+  System.out.println("******************");
+  System.out.println("***               ");
+  System.out.println("***               ");
+  System.out.println("***               ");
+  System.out.println("******************");
+  System.out.println("******************");
+  System.out.println("               ***");
+  System.out.println("               ***");
+  System.out.println("               ***");
+  System.out.println("               ***");
+  System.out.println("******************");
+  System.out.println("******************");
+
+}
+
+static void H(){
+  clrscr();
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("******************");
+  System.out.println("******************");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+  System.out.println("***            ***");
+
+}
+
+public static void main(String args[]){
+ U();T();K();A();R();S();H();  
+}
+
+static void clrscr(){
+ try{
+  Thread.sleep(1500);
+ }
+ catch(Exception e){
+ 
+ }
+ System.out.println("\033[H\033[2J");
+}
+
+
+
+
+}
