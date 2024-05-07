97502c4a53c643f73c4d54646617e92d36295a48
Utkarsh709 <155797679+Utkarsh709@users.noreply.github.com>
2024-05-07 21:55:15 +0530

    Add files via upload
    scanner class error

---
+++ scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class scanner{
+ public static void main(String args[]){
+  Scanner sc=new Scanner();
+  System.out.println("enter number:");
+  int a=sc.nextInt();
+ }
+
+}
+
